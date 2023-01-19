a34009ecb5d279b46c045d91dd3ea6c9044bdf54
Kaustubh Bhargava <kaustubhbhargava5@gamil.com>
2023-01-19 23:01:43 +0530

    qr

--- qr/views.py
+++ qr/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from .models import QrCode
 def med(request):
     return render(request,'qr/home.html')
 
 def create_qr(request):
-    return render(request,'qr/create_qr.html')
+    if request.method=="POST":
+        subject = request.POST['subject']
+        QrCode.objects.create(url='https://www.tutorialspoint.com/qr-code-generating-website-in-django')
+    qr_code=QrCode.objects.all()
+    return render(request,'qr/create_qr.html',{"qr":qr_code})
 
 def student(request):
     return render(request, 'qr/student.html')
