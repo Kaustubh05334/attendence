c7d281bb2e08496254dc6ba795781197ce113ee8
Vaibhav <vrdugar02@gmail.com>
2023-01-21 18:59:44 +0530

    added some more designs

--- attendence/urls.py
+++ attendence/urls.py
@@ -33,3 +33,5 @@ urlpatterns = [
     path('logout/',logout_page),
     path('forgot/',forgot_pass)
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
