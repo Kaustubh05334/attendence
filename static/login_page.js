8a475331c137f0856ae104131e39573b4ab23263
Kaustubh Bhargava <kaustubhbhargava5@gamil.com>
2023-01-25 14:55:44 +0530

    recent change

---
+++ static/login_page.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".signin-button");
+const modal = document.getElementById('image');
+const form = document.getElementById('form');
+btn.onclick=function(){
+    modal.style.display="block";
+    form.style.display="none";
+}
+
+
